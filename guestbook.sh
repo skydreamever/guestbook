7e417f54ff3ca75de4695c33193f3514423df879
skydreamever <dxs9642@gmail.com>
2017-01-06 11:24:14 +0800

    修改一个问题

--- guestbook.sh
+++ guestbook.sh
@@ -1,11 +1,11 @@
-docker pull registry.cn-hangzhou.aliyuncs.com/skydreamever/guestbook:redis
-docker tag registry.cn-hangzhou.aliyuncs.com/skydreamever/guestbook:redis  gcr.io/google_containers/redis:e2e
-docker rmi registry.cn-hangzhou.aliyuncs.com/skydreamever/guestbook:redis
+docker pull registry.cn-hangzhou.aliyuncs.com/skydreamever/guestbook:e2e
+docker tag registry.cn-hangzhou.aliyuncs.com/skydreamever/guestbook:e2e  gcr.io/google_containers/redis:e2e
+docker rmi registry.cn-hangzhou.aliyuncs.com/skydreamever/guestbook:e2e
 
-docker pull registry.cn-hangzhou.aliyuncs.com/skydreamever/guestbook:gb-redisslave
-docker tag registry.cn-hangzhou.aliyuncs.com/skydreamever/guestbook:gb-redisslave  gcr.io/google_samples/gb-redisslave:v1
-docker rmi registry.cn-hangzhou.aliyuncs.com/skydreamever/guestbook:gb-redisslave
+docker pull registry.cn-hangzhou.aliyuncs.com/skydreamever/guestbook:v1
+docker tag registry.cn-hangzhou.aliyuncs.com/skydreamever/guestbook:v1  gcr.io/google_samples/gb-redisslave:v1
+docker rmi registry.cn-hangzhou.aliyuncs.com/skydreamever/guestbook:v1
 
-docker pull registry.cn-hangzhou.aliyuncs.com/skydreamever/guestbook:gb-frontend
-docker tag registry.cn-hangzhou.aliyuncs.com/skydreamever/guestbook:gb-frontend  gcr.io/google-samples/gb-frontend:v4
-docker rmi registry.cn-hangzhou.aliyuncs.com/skydreamever/guestbook:gb-frontend
+docker pull registry.cn-hangzhou.aliyuncs.com/skydreamever/guestbook:v4
+docker tag registry.cn-hangzhou.aliyuncs.com/skydreamever/guestbook:v4  gcr.io/google-samples/gb-frontend:v4
+docker rmi registry.cn-hangzhou.aliyuncs.com/skydreamever/guestbook:v4
